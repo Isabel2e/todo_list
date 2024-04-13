d556d4ac1a53982dbb54728a127d9a875fb715e1
Isabel2e <2202056@utrivieramaya.edu.mx>
2024-04-12 20:27:44 -0500

    Parte2

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,7 @@
 import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:flutter/material.dart';
 import 'package:firebase_core/firebase_core.dart';
+import 'package:todo_list/view/form_task.dart';
 import 'firebase_options.dart';
 
 void main() async {
@@ -58,7 +59,10 @@ class _MyHomePageState extends State<MyHomePage> {
         }),
       floatingActionButton: FloatingActionButton(
         onPressed: () {
-          print('Hola');
+          Navigator.push(
+            context,
+            MaterialPageRoute(builder: (context)=> const AddNewTaskScreen())
+          );
         },
         tooltip: 'Agregar nueva tarea',
         child: const Icon(Icons.add),
